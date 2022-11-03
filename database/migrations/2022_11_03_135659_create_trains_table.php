7e814ab8a7f162cb468fc52bf37ec675a9746ccf
Fraianni <francescoianni1995@gmail.com>
2022-11-03 15:07:13 +0100

    Creazione progetto e creazione tabella

---
+++ database/migrations/2022_11_03_135659_create_trains_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('Azienda');
+            $table->string('Partenza');
+            $table->string('Arrivo');
+            $table->dateTime('Orario partenza', $precision = 0);
+            $table->dateTime('Orario arrivo', $precision = 0);
+            $table->integer('Codice treno');
+            $table->integer('Numero carrozze');
+            $table->boolean('In orario');
+            $table->boolean('Cancellato');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
+
+/*
+Azienda
+Stazione di partenza
+Stazione di arrivo
+Orario di partenza
+Orario di arrivo
+Codice Treno
+Numero Carrozze
+In orario
+Cancellato
+*/
